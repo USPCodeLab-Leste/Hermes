cd6bbf30e153727b6321ccba6475116fc8f88915
Douglas <Douglas1412@usp.br>
2026-01-09 16:58:13 -0300

    Refactor login page to single dynamic form
    Merged login, register, and email check steps into a single form with dynamic fields and improved transitions. Updated button labels and input states based on the current step, and replaced icon images with anchor tags in the footer for better accessibility.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -25,133 +25,126 @@ export default function Login() {
     return false
   }
 
-  const handleNextStep = (e: React.FormEvent) => {
+  const handleFormSubmit = (e: React.FormEvent) => {
     e.preventDefault()
 
-    if (!email) {
-      alert("Digite um email!")
+    if (step === 'check_email') {
+      if (!email) {
+        alert("Digite um email!")
+        return
+      }
+      const exists = checkEmailExists(email)
+      setStep(exists ? 'login' : 'register')
       return
     }
 
-    const exists = checkEmailExists(email)
+    // 2. Fase de Login
+    if (step === 'login') {
+      login('dummy-token')
+      return
+    }
 
-    if (exists) {
-      setStep('login') // Carrega tela de login
-    } else {
-      setStep('register') // Carrega tela de cadastro
+    // 3. Fase de Cadastro
+    if (step === 'register') {
+      alert("Conta criada com sucesso! (Simulação)")
+      //Adcionar a chamada de cadastro aqui
+      return
     }
   }
 
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault()
-    login('dummy-token')
-  }
-
-  const handleRegister = (e: React.FormEvent) => {
-    e.preventDefault()
-    alert("Conta criada com sucesso! (Simulação)")
-    // ADICIONAR LÓGICA DE CADASTRO AQUI
-  }
 
   return (
     <>
       <main className="min-h-screen w-full flex flex-col md:flex-row bg-amber-100">
 
         {/* LOGO E DESCRIÇÃO DO HERMES */}
-        <section className="flex-1 md:w-1/2 w-full flex flex-col justify-center items-center gap-6 p-4">
+        <section className="flex-1 md:w-1/2 w-full flex flex-col justify-center items-center p-4">
           <img src={logoHermes} alt="Logo do Hermes" className="w-48 md:w-64 max-w-xs object-contain" />
 
-          <p className="text-base md:text-center max-w-md">
-            Lorem ipsum dolor sit amet, consectet adipiscing elit. Cras finibus  vehicula nulla quis maximus. Nullam non augue est. Mauris et placerat  augue.
-          </p>
+          <div className={`grid transition-all duration-500 ease-in-out 
+            ${step === 'register'? 'grid-rows-[0fr] opacity-0 margin-0': 'grid-rows-[1fr] opacity-100'}`
+          }>
+            <div className="overflow-hidden pt-6">
+              <p className="text-base md:text-center max-w-md pb-1">
+                Lorem ipsum dolor sit amet, consectet adipiscing elit. Cras finibus vehicula nulla quis maximus. Nullam non augue est. Mauris et placerat augue.
+              </p>
+            </div>
+          </div>
         </section>
 
         {/* FORMULÁRIO DE LOGIN E Rodapé */}
-        <section className="flex-1 md:w-1/2 w-full flex flex-col justify-between bg-amber-600 p-4 rounded-t-3xl md:rounded-t-none md:rounded-l-3xl">
-          {step === 'check_email' && (
-            <form onSubmit={handleNextStep} className="flex flex-col pt-2 gap-4">
+        <section className="flex-1 md:w-1/2 w-full flex flex-col justify-between bg-amber-700 p-4 rounded-t-3xl md:rounded-t-none md:rounded-l-3xl">
 
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Insira o seu e-mail
-                </label>
-                <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail USP" className="border-3 border-white rounded-2xl p-2 text-white" />
+          <form onSubmit={handleFormSubmit} className="flex flex-col pt-2 gap-3">
+
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email" className="text-white text-base font-semibold">
+                Insira o seu e-mail
+              </label>
+              <input
+                type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} readOnly={step !== 'check_email'} placeholder="E-mail USP"
+                className={`border-3 border-white rounded-2xl p-2 text-white transition-colors duration-300 ${step !== 'check_email' ? 'bg-white/10 cursor-not-allowed' : 'bg-transparent'}`}
+              />
+            </div>
+
+
+            <div className={`grid transition-[grid-template-rows] duration-500 ease-out 
+                ${step === 'check_email' ? 'grid-rows-[0fr]' : 'grid-rows-[1fr]'}`
+            }>
+              <div className="overflow-hidden">
+
+                <div className={`flex flex-col gap-3 transition-opacity duration-500 delay-100 
+                    ${step === 'check_email' ? 'opacity-0' : 'opacity-100'}`}>
+
+                  {/* CAMPOS DE LOGIN */}
+                  {step === 'login' && (
+                    <div className="flex flex-col gap-1">
+                      <label htmlFor="pass-login" className="text-white text-base font-semibold">
+                        Insira a sua senha
+                      </label>
+                      <input type="password" id="pass-login" placeholder="Senha" className="border-3 border-white rounded-2xl p-2 text-white bg-transparent" />
+                    </div>
+                  )}
+
+                  {/* CAMPOS DE REGISTRO */}
+                  {step === 'register' && (
+                    <>
+                      <div className="flex flex-col gap-1">
+                        <label className="text-white text-base font-semibold">Número USP</label>
+                        <input type="text" placeholder="Número USP" className="border-3 border-white rounded-2xl p-2 text-white bg-transparent" />
+                      </div>
+                      <div className="flex flex-col gap-1">
+                        <label className="text-white text-base font-semibold">Crie uma senha</label>
+                        <input type="password" placeholder="Senha" className="border-3 border-white rounded-2xl p-2 text-white bg-transparent" />
+                      </div>
+                      <div className="flex flex-col gap-1">
+                        <label className="text-white text-base font-semibold">Confirme a senha</label>
+                        <input type="password" placeholder="Senha" className="border-3 border-white rounded-2xl p-2 text-white bg-transparent" />
+                      </div>
+                    </>
+                  )}
+                </div>
               </div>
+            </div>
 
-              <button className="w-2/3 text-white text-lg font-bold bg-amber-300 p-2 rounded-2xl  m-auto">
-                Próximo
-              </button>
-            </form>
-          )}
-
-          {step === 'login' && (
-            <form onSubmit={handleLogin} className="flex flex-col pt-2 gap-4">
-
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Insira o seu e-mail
-                </label>
-                <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail USP" className="border-3 border-white rounded-2xl p-2 text-white" />
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Insira a sua senha
-                </label>
-                <input type="password" id="password" placeholder="Senha" className="border-3 border-white rounded-2xl p-2 text-white" />
-              </div>
-
-              <button className="w-2/3 text-white text-lg font-bold bg-amber-300 p-2 rounded-2xl  m-auto">
-                Entrar
-              </button>
-            </form>
-          )}
-
-          {step === 'register' && (
-            <form onSubmit={handleRegister} className="flex flex-col pt-2 gap-3">
-
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Insira o seu e-mail
-                </label>
-                <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail USP" className="border-3 border-white rounded-2xl p-2 text-white" />
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Núemro USP
-                </label>
-                <input type="text" placeholder="Número USP" className="border-3 border-white rounded-2xl p-2 text-white" />
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Crie uma senha
-                </label>
-                <input type="password" placeholder="Senha" className="border-3 border-white rounded-2xl p-2 text-white" />
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="text-white text-base font-semibold">
-                  Confirme a senha
-                </label>
-                <input type="password" placeholder="Senha" className="border-3 border-white rounded-2xl p-2 text-white" />
-              </div>
-
-              <button className="w-2/3 text-white text-lg font-bold bg-amber-300 p-2 rounded-2xl  m-auto">
-                Criar Conta
-              </button>
-            </form>
-          )}
-
+            <button className="w-2/3 text-white text-lg font-bold bg-amber-400 p-2 mb-4 rounded-2xl m-auto 
+              hover:bg-amber-500 transition-colors z-10">
+              {step === 'check_email' ? 'Próximo' : (step === 'login' ? 'Entrar' : 'Criar Conta')}
+            </button>
+          </form>
 
           <footer className="flex flex-col items-center gap-2 mb-2">
             <p className="text-white text-sm font-bold">Criado por Codelab Leste</p>
             <div className="flex justify-center gap-8">
-              <img src={youtubeIcon} alt="YouTube" className="w-6 h-6 brightness-0 invert opacity-80 hover:opacity-100 cursor-pointer transition-opacity" />
-              <img src={discordIcon} alt="Discord" className="w-6 h-6 brightness-0 invert opacity-80 hover:opacity-100 cursor-pointer transition-opacity" />
-              <img src={instagramIcon} alt="Instagram" className="w-6 h-6 brightness-0 invert opacity-80 hover:opacity-100 cursor-pointer transition-opacity" />
-
+              <a href="" target="_blank">
+                <img src={youtubeIcon} alt="YouTube" className="w-6 h-6 brightness-0 invert opacity-80 hover:opacity-100 cursor-pointer transition-opacity" />
+              </a>
+              <a href="" target="_blank">
+                <img src={discordIcon} alt="Discord" className="w-6 h-6 brightness-0 invert opacity-80 hover:opacity-100 cursor-pointer transition-opacity" />
+              </a>
+              <a href="" target="_blank">
+                <img src={instagramIcon} alt="Instagram" className="w-6 h-6 brightness-0 invert opacity-80 hover:opacity-100 cursor-pointer transition-opacity" />
+              </a>
             </div>
           </footer>
         </section>
