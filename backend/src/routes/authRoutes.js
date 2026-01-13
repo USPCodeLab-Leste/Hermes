7a68521a96f96148ebf3289e1353afafcfccfb18
eduardo <11eduardoalmeida@gmail.com>
2026-01-13 13:46:34 -0300

    feat: implementando servico de email com nodemailer

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
 import { verifyEmail } from "../controllers/authController.js";
+import { sendVerificationEmail } from "../services/emailService.js";
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
 router.get('/verify-email', verifyEmail);
+
+
+// rota teste 
+router.get('/teste-envio', async (req,res) => {
+    try {
+        const emailFake = "bixo2026@usp.br";
+        const token = jwt.sign({email: emailFake }, process.env.JWT_EMAIL_SECRET || 'segredinho secreto'); // token qualquer so pra testar o link
+        await sendVerificationEmail(emailFake,token);
+        res.send("<h1>E-mail enviado com sucesso!<h1> <p>Verifique a url no console pra verificar o email<p>");
+    } catch (error) {
+        res.status(500).send("Houve um error ao enviar: " + error.message);
+    }
+
+});
 
 export default router;
