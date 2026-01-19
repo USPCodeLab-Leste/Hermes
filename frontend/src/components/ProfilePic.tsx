ba5848d0d0f6b96016692b251b80ef0fc0602db3
nichioka <kaua.nichioka@usp.br>
2026-01-19 18:36:59 -0300

    Header 2.0

--- frontend/src/components/ProfilePic.tsx
+++ frontend/src/components/ProfilePic.tsx
@@ -1,5 +1,5 @@
 export default function ProfilePic(){
 	return(
-		<img src="../assets/temp-logo.png" className="max-h-[120px] max-w-[120px] w-[30%] h-[30%] rounded-full" ></img> 
+		<img src="src/assets/temp-logo.png" className="max-h-[120px] max-w-[120px] w-[30%] h-[30%] rounded-full" ></img> 
 	)
 }
