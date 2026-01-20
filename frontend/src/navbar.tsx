9015661490b63acf23ec37ef6bc6b65476ebaae6
Daniyutak <dtakara1101@gmail.com>
2026-01-19 22:54:40 -0300

    add: base da navbar

---
+++ frontend/src/navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <nav>
+
+        <a href="/" className=" rounded-full ">
+        Home
+        </a>
+
+        <a href="/info" className=" rounded-full ">
+        Info
+        </a>
+
+        <a href="/perfil" className=" rounded-full ">
+        Perfil
+        </a>
+
+    </nav>
+  )
+}
