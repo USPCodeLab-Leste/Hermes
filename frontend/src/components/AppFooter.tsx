7f497ff0e40f137752fabf7d196a9e353ab9a06d
Daniyutak <dtakara1101@gmail.com>
2026-01-19 23:25:12 -0300

    add: icons

--- frontend/src/components/AppFooter.tsx
+++ frontend/src/components/AppFooter.tsx
@@ -1,4 +1,7 @@
 import { NavLink } from "react-router-dom";
+import { IconHome } from '@tabler/icons-react';
+import { IconUser } from '@tabler/icons-react';
+import { IconBook2 } from '@tabler/icons-react';
 
 // NavLink pode usar className com uma função para estilizar o link ativo
 // Exemplo: <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
@@ -8,9 +11,13 @@ export default function AppFooter() {
   return (
     <footer className="p-4">
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/info">Informações</NavLink>
-        <NavLink to="/perfil">Perfil</NavLink>
+
+        <NavLink to="/"> <IconHome/> </NavLink>
+        
+        <NavLink to="/info"><IconBook2/></NavLink>
+
+        <NavLink to="/perfil"><IconUser/></NavLink>
+
       </nav>
     </footer>
   )
