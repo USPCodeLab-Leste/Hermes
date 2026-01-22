e33597863bac11bd3cf8add25d367929539bf257
Sapin07 <sapinleste@outlook.com>
2026-01-22 13:56:03 -0300

    refactor: aplicação do estilo/dark mode e refatoração nos buttons

--- frontend/src/components/PerfilButton.tsx
+++ frontend/src/components/PerfilButton.tsx
@@ -1,5 +1,5 @@
 import type { MouseEventHandler, ReactNode } from "react";
-import rightArrow from "../assets/icons/RightArrow.svg";
+import RightArrow from "../assets/icons/RightArrow.svg?react";
 
 interface PerfilButtonProps {
     icon: ReactNode,
@@ -14,14 +14,14 @@ export default function PerfilButton({
 }: PerfilButtonProps){
     return (
         <button
-            className=""
+            className="bg-violet-light text-[20px]/[24px] flex items-center justify-between max-w-95 w-[90%] h-11.25 border-2 px-2 py-4 rounded-2xl mb-4 mx-6"
             onClick={onClick}
         >
-            {icon}
-            <span>
-                {btnName}
+            <span className="flex gap-2 ml-4">
+                {icon}
+                <span className="w-40 text-left">{btnName}</span>
             </span>
-            <img src={rightArrow} alt="" aria-hidden="true"/>
+            <RightArrow className="w-6 h-5 mr-3.5"/>
         </button>
     );
 }
--- frontend/src/pages/Perfil.tsx
+++ frontend/src/pages/Perfil.tsx
@@ -1,50 +1,70 @@
 import { useSignOut } from "../hooks/useSignOut";
+import { useTheme } from "../hooks/useTheme";
 import { auth } from "../services/auth";
 import PerfilButton from "../components/PerfilButton";
+import type { MouseEventHandler, SVGProps, ComponentType } from "react";
 
 // icones
-import userIcon from "../assets/icons/userIcon.svg";
-import darkModeIcon from "../assets/icons/DarkModeIcon.svg";
-import bellIcon from "../assets/icons/bellIcon.svg"
-import bugIcon from "../assets/icons/bugIcon.svg";
-import infoIcon from "../assets/icons/infoIcon.svg";
-import passwordIcon from "../assets/icons/passwordIcon.svg"
-import logoutIcon from "../assets/icons/tabler_logout.svg"
+import UserIcon from "../assets/icons/userIcon.svg?react";
+import DarkModeIcon from "../assets/icons/DarkModeIcon.svg?react";
+import BellIcon from "../assets/icons/bellIcon.svg?react"
+import BugIcon from "../assets/icons/bugIcon.svg?react";
+import InfoIcon from "../assets/icons/infoIcon.svg?react";
+import PasswordIcon from "../assets/icons/passwordIcon.svg?react"
+import LogoutIcon from "../assets/icons/tabler_logout.svg?react"
+import LeftArrow from "../assets/icons/chevron-left.svg?react"
 
 export default function Perfil() {
   const [signOut] = useSignOut(auth);
+  const { toggleTheme } = useTheme();
 
   // onClick Functions (alterar para as funcoes ou router corretos)
   const handlePerfil = () => console.log("Perfil ok");
   const handleSenha = () => console.log("Senha ok");
   const handleNotificacoes = () => console.log("Notificacoes");
-  const handleDarkMode = () => {}; //() => setDarkMode
+  const handleDarkMode = toggleTheme; 
   const handleBug = () => {};
   const handleInfo = () => {};
   //seria interessante criar um interface para esses objetos
-  const actions = [
-    { label: "Gerenciar Perfil", icon: userIcon, onClick: handlePerfil },
-    { label: "Alterar Senha", icon: passwordIcon, onClick: handleSenha },
-    { label: "Notificações", icon: bellIcon, onClick: handleNotificacoes },
-    { label: "Dark Mode", icon: darkModeIcon, onClick: handleDarkMode },
-    { label: "Relatar Bugs", icon: bugIcon, onClick: handleBug },
-    { label: "Informações", icon: infoIcon, onClick: handleInfo },
-    { label: "Sair", icon: logoutIcon, onClick: signOut },
+
+  interface PerfilAction {
+    label: string
+    icon: ComponentType<SVGProps<SVGSVGElement>>,
+    onClick: MouseEventHandler<HTMLButtonElement>,
+  }
+
+
+  const actions: PerfilAction[] = [
+    { label: "Gerenciar Perfil", icon: UserIcon, onClick: handlePerfil },
+    { label: "Alterar Senha", icon: PasswordIcon, onClick: handleSenha },
+    { label: "Notificações", icon: BellIcon, onClick: handleNotificacoes },
+    { label: "Dark Mode", icon: DarkModeIcon, onClick: handleDarkMode },
+    { label: "Relatar Bugs", icon: BugIcon, onClick: handleBug },
+    { label: "Informações", icon: InfoIcon, onClick: handleInfo },
+    { label: "Sair", icon: LogoutIcon, onClick: signOut },
   ];
 
   return (
-    <section>
+    <section className=" flex flex-col items-center">
+      <div className="w-screen flex items-center flex-col text-paper dark:text-paper bg-violet-dark  mb-6 pb-5.5 gap-5 rounded-b-2xl">
+        <div className="mt-6 mp-5 mx-4 flex justify-center w-screen">
+          <LeftArrow className="w-6 h-6 absolute ml-4 left-0"/>
+          <h6 className="text-[20px]/[24px] ">Perfil</h6>
+        </div>
+       {/* <img src="" alt="iamgem de perfil" />*/}
+       <div className="flex flex-col justify-center items-center">
+          <h5 className="text-[24px]/[28px] font-bold">Code Laber da Silva</h5>
+          <span className="text-[16px]/[20px]">email@email.com</span>
+       </div>
+        
+      </div>
       
-      <p>Esta é a página de perfil do usuário.</p>
-      <img src="" alt="iamgem de perfil" />
-      <h5>User name</h5>
-      <p className="Subtitle">email@email.com</p>
-
       {actions.map(action => (
+        
         <PerfilButton
           key={action.label}
           btnName={action.label}
-          icon={<img src={action.icon} alt="" aria-hidden="true" />}
+          icon={<action.icon className="w-6 h-5" />}
           onClick={action.onClick}
         />
       ))}
