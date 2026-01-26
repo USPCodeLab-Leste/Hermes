37837285ee783c92e1524edaa71c6fcb83db5592
Daniyutak <dtakara1101@gmail.com>
2026-01-26 19:57:51 -0300

    Fix: aumenta o circulo da navbar pra ficar mais fiel ao figma

--- frontend/src/components/AppFooter.tsx
+++ frontend/src/components/AppFooter.tsx
@@ -6,19 +6,22 @@ import { IconBook2 } from '@tabler/icons-react';
 // NavLink pode usar className com uma função para estilizar o link ativo
 // Exemplo: <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
 
+
 export default function AppFooter() {
- 
+
   return (
     <footer className="w-full bottom-0">
+
       <nav className="bg-violet-dark max-w h-16 flex justify-evenly items-center">
  
-        <NavLink to="/" className={({ isActive }) => isActive ? ' bg-teal-mid rounded-full p-2 -translate-y-6' : 'rounded-full p-2'}> <IconHome/> </NavLink>
+        <NavLink to="/" className={({ isActive }) => isActive ? ' bg-teal-mid rounded-full p-4 -translate-y-6' : 'rounded-full p-2'}> <IconHome/> </NavLink>
     
-        <NavLink to="/info" className={({ isActive }) => isActive ? ' bg-teal-mid rounded-full p-2 -translate-y-6' : 'rounded-full p-2 '}><IconBook2/></NavLink>
+        <NavLink to="/info" className={({ isActive }) => isActive ? ' bg-teal-mid rounded-full p-4 -translate-y-6' : 'rounded-full p-2 '}><IconBook2/></NavLink>
 
-        <NavLink to="/perfil" className={({ isActive }) => isActive ? ' bg-teal-mid rounded-full p-2 -translate-y-6' : 'rounded-full p-2 '}><IconUser/></NavLink>
-      
+        <NavLink to="/perfil" className={({ isActive }) => isActive ? ' bg-teal-mid rounded-full p-4 -translate-y-6' : 'rounded-full p-2 '}><IconUser/></NavLink>
       </nav>
+
+
     </footer>
   )
 }
