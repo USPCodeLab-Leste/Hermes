bd485f1cc8930d34630b3da35676ab320c420e96
webcaique <sidraocaique@gmail.com>
2026-01-26 11:14:32 -0300

    feat persquisar o evento, em que vai pesquisar tanto por id, quanto por outro textos

--- backend/src/controllers/events.controller.js
+++ backend/src/controllers/events.controller.js
@@ -3,7 +3,17 @@ import eventsModel from "../models/events.model.js";
 class eventsController {
 
   async getEvents(req, res) {
+    try {
+      const events = eventsModel.searchEvent(req.params.id);
 
+      if(events == undefined) {
+        return res.status(400).json({error: "Events not found"});
+      }
+
+      return res.status(200).json(events);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async postEvents(req, res) {
--- backend/src/models/events.model.js
+++ backend/src/models/events.model.js
@@ -1,3 +1,5 @@
+import pool from "../config/database.js";
+
 class eventsModel {
   
   // Precisa devolver com base em um limite e tags
@@ -14,6 +16,59 @@ class eventsModel {
 
   }
 
+  async searchEvent(search = undefined) {
+    let query = `
+      SELECT *
+      FROM post
+      JOIN post_tag. ON post_tag.post_id = post.id
+      JOIN tag on post_tag.id_tag = tag.id
+    `;
+
+    const values = [];
+
+    if(search != undefined) {
+      query += ` WHERE post.id = $1 OR post.local = $1 OR post.titulo = $1`
+      values.push(search);
+    }
+
+    query += `;`;
+
+
+    const result = await
+    pool
+    .query(query, values)
+    .then( res => res.rows[0])
+    .catch( err => console.error("ERROR GET EVENTS: ", err));
+
+    return result;
+
+
+  }
+
 }
 
 export default new eventsModel();
+
+/*
+
+let query = `
+SELECT *
+FROM post
+JOIN post_tag on post_tag.idPost = post.idPost
+JOIN tag on tag.tag_id = post_tag.tag_id
+`;
+const values = [tags, limit || "ALL"];
+if(tags != []){
+  query += ` WHERE tag.title = ANY($1)`;
+}
+query += ` LIMIT $2`;
+
+const result = await
+pool
+.query(query, values)
+.then( res => res.rows[0])
+.catch( err => console.error("ERROR GET EVENTS ON SEARCH: ", err));
+
+return result;
+
+*/
