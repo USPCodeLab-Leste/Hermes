7a6abb542b8c848abf4e149e7844e99e45ab18e9
Caique Costa <ccostafrias@gmail.com>
2026-01-27 12:53:43 -0300

    feat: adiciona animação no perfil
    - muda a `key` dos buttons para usar id;
    - centraliza botões na section.

--- frontend/src/pages/Perfil.tsx
+++ frontend/src/pages/Perfil.tsx
@@ -16,6 +16,30 @@ import PasswordIcon from "../assets/icons/passwordIcon.svg?react"
 import LogoutIcon from "../assets/icons/tabler_logout.svg?react"
 import LeftArrow from "../assets/icons/chevron-left.svg?react"
 import LightModeIcon from "../assets/icons/sun.svg?react"
+import { motion, stagger, type Variants } from "framer-motion";
+
+const variants: Variants = {
+  visible: { 
+    transition: {
+      delayChildren: stagger(0.05),
+    },
+  }
+}
+
+const variantsChild: Variants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { 
+    opacity: 1, 
+    y: 0,
+    transition: { 
+      // ease: "easeOut", 
+      // duration: 0.3,
+      type: 'spring',
+      stiffness: 320,
+      damping: 20,
+    }
+  },
+}
 
 export default function Perfil() {
   const [signOut] = useSignOut(auth);
@@ -33,19 +57,20 @@ export default function Perfil() {
 
   //seria interessante criar um interface para esses objetos: feito
   interface PerfilAction {
+    id: number,
     label: string
     icon: ComponentType<SVGProps<SVGSVGElement>>,
     onClick: MouseEventHandler<HTMLButtonElement>,
   }
 
-  let actions: PerfilAction[] = [
-    { label: "Gerenciar Perfil", icon: UserIcon, onClick: handlePerfil },
-    { label: "Alterar Senha", icon: PasswordIcon, onClick: handleSenha },
-    { label: "Notificações", icon: BellIcon, onClick: handleNotificacoes },
-    { label: isDark ? "Modo Claro" : "Modo Escuro", icon: isDark ? LightModeIcon : DarkModeIcon, onClick: handleDarkMode },
-    { label: "Relatar Bugs", icon: BugIcon, onClick: handleBug },
-    { label: "Informações", icon: InfoIcon, onClick: handleInfo },
-    { label: "Sair", icon: LogoutIcon, onClick: signOut },
+  const actions: PerfilAction[] = [
+    { id: 0, label: "Gerenciar Perfil", icon: UserIcon, onClick: handlePerfil },
+    { id: 1, label: "Alterar Senha", icon: PasswordIcon, onClick: handleSenha },
+    { id: 2, label: "Notificações", icon: BellIcon, onClick: handleNotificacoes },
+    { id: 3, label: isDark ? "Modo Claro" : "Modo Escuro", icon: isDark ? LightModeIcon : DarkModeIcon, onClick: handleDarkMode },
+    { id: 4, label: "Relatar Bugs", icon: BugIcon, onClick: handleBug },
+    { id: 5, label: "Informações", icon: InfoIcon, onClick: handleInfo },
+    { id: 6, label: "Sair", icon: LogoutIcon, onClick: signOut },
   ];
 
   const navigate = useNavigate();
@@ -56,9 +81,9 @@ export default function Perfil() {
     <>
       <header className="flex items-center flex-col text-paper dark:text-paper bg-violet-dark  mb-6 pb-7.5 gap-5 rounded-b-2xl">
         <div className="mt-6 mp-5 mx-4 flex justify-center">
-          <button onClick={() => navigate("/")} className="absolute ml-4 left-0 cursor-pointer" aria-label="voltar">
+          {/* <button onClick={() => navigate("/")} className="absolute ml-4 left-0 cursor-pointer" aria-label="voltar">
             <LeftArrow  className="size-6"/>
-          </button>
+          </button> */}
           <h6 className="text-[20px]/[24px] ">Perfil</h6>
         </div>
         {/* <img src="" alt="iamgem de perfil" />*/}
@@ -68,19 +93,25 @@ export default function Perfil() {
         </div>
         
       </header>
-      <main className="main-app">
-        <section className=" flex flex-col items-center">
+      <main className="main-app flex flex-col justify-center">
+        <motion.section 
+          className="flex flex-col items-center justify-center"
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+        >
           
           {actions.map(action => (
             
             <PerfilButton
-              key={action.label}
+              key={action.id}
               btnName={action.label}
               icon={<action.icon className="w-6 h-5" />}
               onClick={action.onClick}
+              variants={variantsChild}
             />
           ))}
-        </section>
+        </motion.section>
       </main>
     </>
   )
