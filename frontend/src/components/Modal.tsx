013a11203a06409af4d9070c56ab9af1cd5616ab
Caique Costa <ccostafrias@gmail.com>
2026-01-27 23:03:26 -0300

    feat: atualiza modal
    - adiciona drag para poder fechar modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion, useAnimate, useDragControls } from 'framer-motion'
 import { createPortal } from 'react-dom'
 import { FocusTrap } from 'focus-trap-react'
 
@@ -11,9 +11,28 @@ interface ModalProps {
 }
 
 function Modal({ children, onClose, ref }: ModalProps) {
+  const [dragOffset, setDragOffset] = useState(0)
+  const [scope, animate] = useAnimate()
+  const dragControls = useDragControls()
+  
+  const handleDrag = useCallback((_: any, info: any) => {
+    if (info.offset.y < 0) return
+    
+    setDragOffset(info.offset.y)
+  }, [])
+
+  const handleDragEnd = useCallback(() => {
+    if (dragOffset > 150) {
+      onClose()
+      return
+    }
+    animate(scope.current, { y: 0, opacity: 1 }, { type: 'spring', stiffness: 300, damping: 25 })
+    setDragOffset(0)
+  }, [animate, dragOffset, onClose, scope])
+  
   return (
     <motion.div
-      className="modal-open overlay fixed inset-0 bg-black/50 bg-opacity-50 flex justify-center items-center z-50"
+      className="modal-open overlay min-h-dvh fixed inset-0 bg-black/50 bg-opacity-50 flex justify-center items-center z-50"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
@@ -25,26 +44,34 @@ function Modal({ children, onClose, ref }: ModalProps) {
           escapeDeactivates: false,
           clickOutsideDeactivates: true,
           initialFocus: false,
+          fallbackFocus: () => scope.current as HTMLElement,
         }}
       >
         <motion.div
-          className='modal-card'
+          ref={scope}
+          className='p-6 pt-2 bg-violet-mid rounded-t-2xl md:rounded-2xl modal-card absolute 
+                     md:relative bottom-0 w-full md:w-9/10 md:max-w-120 origin-bottom md:origin-center'
           onClick={(e) => e.stopPropagation()}
           initial={{ scale: 0.8, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
+          animate={{ scale: 1, opacity: 1 - Math.abs(dragOffset) / 1000 }}
           exit={{ scale: 0.8, opacity: 0 }}
+          drag="y"
+          dragListener={false}
+          dragControls={dragControls}
+          dragConstraints={{ top: 0, bottom: 0 }}
+          dragElastic={{ top: 0, bottom: 0.5 }}
+          onDrag={handleDrag}
+          onDragEnd={handleDragEnd}
           transition={{ type: 'spring', stiffness: 300, damping: 25 }}
           role="dialog"
           aria-modal="true"
         >
-          <button 
-            onClick={onClose}
-            role="button"
-            aria-label="Close modal"
-            className='cursor-pointer mb-4'
+          <div 
+            className='cursor-grab py-4 px-12 m-auto touch-none active:cursor-grabbing select-none'
+            onPointerDown={(e) => dragControls.start(e)}
           >
-            Close
-          </button>
+            <div className='h-2 w-15 bg-violet-dark rounded-full m-auto'></div>
+          </div>
           {children}
         </motion.div>
       </FocusTrap>
