04cd91255e2774feb160774afdc3147037c74497
Caique Costa <ccostafrias@gmail.com>
2026-02-04 12:22:35 -0300

    feat: adiciona navbar para `InfoLayout`

--- frontend/src/layouts/InfoLayout.tsx
+++ frontend/src/layouts/InfoLayout.tsx
@@ -4,6 +4,7 @@ import { NavLink, Outlet } from 'react-router-dom'
 // Components
 import AppHeader from '../components/AppHeader'
 import SearchBar from "../components/SearchBar";
+import { motion } from 'framer-motion';
 
 export default function InfoLayout() {
   const [search, setSearch] = useState("");
@@ -16,8 +17,8 @@ export default function InfoLayout() {
         </div>
       </AppHeader>
       <main className="main-app">
-        <nav>
-          <ul className="flex gap-4 mb-6">
+        <nav className="outline-1 rounded-full">
+          <ul className="flex items-center gap-4 mb-6 justify-between py-2 px-4">
             <li><NavItem to="estudos" label="Estudos" /></li>
             <li><NavItem to="campus" label="Campus" /></li>
             <li><NavItem to="apoios" label="Apoios" /></li>
@@ -32,8 +33,35 @@ export default function InfoLayout() {
 
 const NavItem = ({ to, label }: { to: string; label: string }) => {
   return (
-    <NavLink to={to}>
-      {label}
+    <NavLink 
+      to={to}
+      className="inline-flex items-center justify-center relative"
+    >
+      {({ isActive }) => (
+        <>
+          {isActive && (
+            <motion.div
+              layoutId="info-indicator"
+              transition={{ 
+                type: "spring", 
+                stiffness: 500, 
+                damping: 30,
+              }}
+              className="absolute z-5"
+            >
+              <span 
+                className='bg-teal-mid relative z-5 py-1 px-3 rounded-full text-transparent'
+                aria-hidden="true"
+              >
+                {label}
+              </span>
+            </motion.div>
+          )}
+          <span className='relative z-6'>
+            {label}
+          </span>
+        </>
+      )}
     </NavLink>
   )
 }
