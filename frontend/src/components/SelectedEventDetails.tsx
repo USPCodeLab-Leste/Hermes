45daaf8f9380b45e12afc800e21ab0ce8b44d958
Caique Costa <ccostafrias@gmail.com>
2026-02-18 15:47:44 -0300

    revert "feat: botão de compartilhar evento"
    This reverts commit 7a7ac6a09858eb27d0f5b232e6d4f8079af442dc.

--- frontend/src/components/SelectedEventDetails.tsx
+++ frontend/src/components/SelectedEventDetails.tsx
@@ -1,5 +1,4 @@
 import { useCallback } from "react"
-import { toast } from "react-toastify"
 
 // Types
 import type { Event } from "../types/events"
@@ -10,38 +9,10 @@ import { DateWrapper } from "./Date"
 import { GenericButton } from "./GenericButton"
 
 export function SelectedEventDetails({ event }: { event: Event | null }) {
-  const handleShare = useCallback(async () => {
-    if (!event) return;
-
-    const shareData = {
-      title: event.title,
-      text: `Confira o evento: ${event.title} em ${event.local}`,
-      url: window.location.href, // TODO: Gerar URL específica do evento.
-    };
-
-    // Verifica se é um dispotivo touch
-    const isTouchDevice = navigator.maxTouchPoints > 0;
-    
-    // Se estiver no celular usa o navigator.share, caso contrário copia o link para o clipboard
-    if (navigator.share && isTouchDevice) {
-      try {
-        // Celular
-        await navigator.share(shareData);
-        return;
-      } catch (error) {
-        if ((error as Error).name === "AbortError") return;
-        console.error("Erro ao compartilhar:", error);
-      }
-    } else {
-      // Fallback para Desktop: Copiar para o Clipboard
-      try {
-        await navigator.clipboard.writeText(shareData.url);
-        toast.success("Link do evento copiado!");
-      } catch {
-        toast.error("Não foi possível copiar o link.");
-      }
-    }
-  }, [event]);
+  // Handlers
+  const handleShare = useCallback(() => {
+    // TODO: implementar função para compartilhar com react-share
+  }, []);
 
   return (
     <div className="flex flex-col gap-4">
@@ -55,17 +26,13 @@ export function SelectedEventDetails({ event }: { event: Event | null }) {
         <div className="flex flex-row flex-wrap gap-x-2 gap-y-0 items-center mb-2">
           <span className="text-[18px] dark:text-paper/75 text-ink/75">{event?.local}</span>
           <span>-</span>
-          <DateWrapper
-            start={event?.data_inicio!}
-            end={event?.data_fim!}
-            textClass="dark:text-paper/75 text-ink/75"
-          />
+          <DateWrapper start={event?.data_inicio!} end={event?.data_fim!} textClass="dark:text-paper/75 text-ink/75"/>
         </div>
-        <p className="text-[16px] dark:text-paper/50 text-ink/50 text-justify hyphens-auto indent-2">
-          {event?.body}
-        </p>
+        <p className="text-[16px] dark:text-paper/50 text-ink/50 text-justify hyphens-auto indent-2">{event?.body}</p>
       </div>
-      <GenericButton onClick={handleShare}>
+      <GenericButton
+        onClick={handleShare}
+      >
         <span className="text-paper">Compartilhar Evento</span>
       </GenericButton>
     </div>
