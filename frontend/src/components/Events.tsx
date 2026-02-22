71fc8038d904bde01743865b21744770b4d3fcf9
Caique Costa <ccostafrias@gmail.com>
2026-02-22 01:29:28 -0300

    fix: previne tags de quebrarem card quando em excesso

--- frontend/src/components/Events.tsx
+++ frontend/src/components/Events.tsx
@@ -101,7 +101,7 @@ interface TagsProps {
 export function Tags({ tags, className }: TagsProps) {
   return (
     <motion.div 
-      className={`flex flex-row gap-2 flex-wrap ${className}`}
+      className={`flex flex-row gap-2 md:flex-wrap overflow-hidden ${className}`}
       variants={tagsVariants}
     >
       {tags.map((tag, index) => (
@@ -240,10 +240,7 @@ const TagWrapper = ({ children, canSelect, className, variants, onClick, disable
 
   return (
     <Component 
-      className={`px-2 md:px-3 py-2 rounded-full text-[12px] md:text-sm font-medium text-paper transition-colors
-                 inline-flex items-center justify-center gap-1 min-w-12 shadow-md shadow-black/20 bg-teal-light 
-                 ${className ?? ''} 
-                 ${disabled ? 'opacity-50 cursor-not-allowed' : canSelect ? 'cursor-pointer' : ''}`}
+      className={`px-2 md:px-3 py-2 rounded-full text-[12px] md:text-sm font-medium text-paper transition-colors inline-flex items-center justify-center gap-1 min-w-12 shadow-md shadow-black/20 bg-teal-light shrink-0 ${className ?? ''} ${disabled ? 'opacity-50 cursor-not-allowed' : canSelect ? 'cursor-pointer' : ''}`}
       layout
       variants={variants} 
       onClick={onClick}
