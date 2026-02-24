78e6a425ebd09213253cfad536f940d1bbc7eab5
Caique Costa <ccostafrias@gmail.com>
2026-02-24 13:39:21 -0300

    feat: atualiza e cria novos types para payload, response, user e erro

--- frontend/src/types/payloads.ts
+++ frontend/src/types/payloads.ts
@@ -8,3 +8,12 @@ export interface RegisterPayload {
   email: string
   password: string
 }
+
+export interface ChangeName {
+  name: string
+}
+
+export interface ChangePassword {
+  oldPassword: string
+  newPassword: string
+}
