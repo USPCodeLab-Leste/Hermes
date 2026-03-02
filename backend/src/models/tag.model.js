f0b32947ac43c1b5cbdc9d7dadf523b1cd293f99
Kaua Moreno <kaua.moreno2005@gmail.com>
2026-03-02 15:43:37 -0300

    fix(tag): Retornando funcao findByName

--- backend/src/models/tag.model.js
+++ backend/src/models/tag.model.js
@@ -15,6 +15,18 @@ class TagModel {
     return result.rows[0];
   }
 
+  async findByName(name) {
+    const query = `
+      SELECT *
+      FROM tb_tag
+      WHERE LOWER(name) = LOWER($1)
+      LIMIT 1
+    `;
+
+    const result = await pool.query(query, [name]);
+    return result.rows[0];
+  }
+
   // pra criar tag
   async create(name, type) {
     const id = crypto.randomUUID();
