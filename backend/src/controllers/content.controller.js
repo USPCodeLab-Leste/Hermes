608e20f689537dd4a72d5435fa524efaa5e3ab72
eduardo <11eduardoalmeida@gmail.com>
2026-03-12 15:32:27 -0300

    feat: adiciona validacao de autoria para edicao e exclusao de conteudo

--- backend/src/controllers/content.controller.js
+++ backend/src/controllers/content.controller.js
@@ -130,6 +130,16 @@ class BaseContentController {
         return res.status(400).json({ error: "ID não informado" });
       }
 
+      const contentCheck = await ContentModel.findById(id);
+
+      if (!contentCheck) {
+        return res.status(404).json({ error: `${this.type} não encontrado` });
+      }
+
+      if (contentCheck.autor_id !== userId) {
+        return res.status(403).json({ error: "acesso negado: Você não é o autor deste conteúdo."});
+      }
+
       const body = this.schema.update
         ? this.schema.update.parse(req.body)
         : req.body;
@@ -172,6 +182,7 @@ class BaseContentController {
   async getById(req, res) {
     try {
       const id = req.params.id;
+      const userId = req.user.id;
 
       if (!id) {
         return res.status(400).json({ error: "ID não informado" });
@@ -180,9 +191,11 @@ class BaseContentController {
       const response = await ContentModel.findById(id);
 
       if (!response) {
-        return res.status(404).json({
-          error: `${this.type} não encontrado`
-        });
+        return res.status(404).json({ error: `${this.type} não encontrado` });
+      }
+
+      if (!response.autor_id != userId) {
+        return res.status(403).json({ error: "acesso negado: Você não é o autor deste conteúdo"});
       }
 
       const cleanedData = this.removeUnwantedFields(
@@ -207,8 +220,18 @@ class BaseContentController {
       if (!id) {
         return res.status(400).json({ error: "ID não informado" });
       }
+      
+      const contentCheck = await ContentModel.findById(id);
 
-      const response = await ContentModel.delete(id, userId);
+      if (!contentCheck) {
+        return res.status(404).json({ error: `${this.type} não encontrado`});
+      }
+
+      if (contentCheck.autor_id !== userId) {
+        return res.status(403).json({ error: "acesso negado: Você não é o autor deste conteúdo"});
+      }
+      
+      const response = await ContentModel.delete(id);
 
       if (!response) {
         return res.status(400).json({
