958ba4564e5353c178e5fa8cb52f034ab03c72f3
Kaua Moreno <kaua.moreno2005@gmail.com>
2026-04-21 01:24:37 -0300

    fix: Removendo query desnecessaria

--- backend/src/models/content.model.js
+++ backend/src/models/content.model.js
@@ -183,10 +183,6 @@ class ContentModel {
     }
 
     if (onlyFuture) {
-      query += ` AND p.data_inicio IS NOT NULL AND p.data_inicio >= CURRENT_DATE`;
-    }
-
-    if (onlyFuture) {
       query += ` AND p.data_fim IS NOT NULL AND p.data_fim >= CURRENT_DATE`;
     }
 
